fe923ec802a7393a29015ad9e8f957f4c9b695f3
AndreeLukashiano <andree.20095@mhs.unesa.ac.id>
2022-11-07 09:12:28 +0700

    first commit

---
+++ proyek_uas_pemmob/lib/constants.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class Constants {
+  //Primary color
+  static var primaryColor = const Color(0xff296e48);
+  static var blackColor = Colors.black54;
+
+  //Onboarding texts
+  static var titleOne = "MANGANREK";
+  static var descriptionOne = "MANGAN MANGAN MANGAN MANGAN.";
+  static var titleTwo = "MANGAN REK";
+  static var descriptionTwo = "MANGAN MANGAN MANGAN.";
+  static var titleThree = "MANGAN YA REK";
+  static var descriptionThree = "MANGAN MANGAN.";
+}
---
+++ proyek_uas_pemmob/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'ui/onboarding_screen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'UAS PEMMOB',
+      home: OnboardingScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
